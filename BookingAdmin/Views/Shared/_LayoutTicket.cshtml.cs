17e9650a9b2a9c40214896e4ddbc60197bc1aa0b
Danny Linh <117795000+thanh210925@users.noreply.github.com>
2025-11-03 03:31:39 +0700

    chỉnh giao diện

---
+++ BookingAdmin/Views/Shared/_LayoutTicket.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookingAdmin.Views.Shared
+{
+    public class _LayoutTicketModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
